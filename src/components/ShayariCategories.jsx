b29ad07cad570831d4617df5dea943e9d73d5d0e
prabh-ux <prabhkiratsinghbusiness@gmail.com>
2025-03-16 16:13:15 +0530

    fixed shayari categorin

--- src/components/ShayariCategories.jsx
+++ src/components/ShayariCategories.jsx
@@ -5,15 +5,17 @@ const ShayariCategories = () => {
   return (
     <section className="bg-white py-6 px-[1rem] text-center">
         <h2 className="text-2xl font-semibold text-center ">💖 Explore More Punjabi Shayari Categories</h2>
-        <div className="flex flex-wrap justify-center gap-4 mt-6">
+        <div className="flex flex-wrap justify-center gap-4 mt-6 min-h-[100px]">
+
           {categories.map((category) => (
-            <Link
-              key={category.path} // Unique key should be 'path'
-              to={category.path.replace(":pageNo","1")} // Use the 'path' property correctly
-              className="px-6 py-3 bg-red-500 text-white rounded-lg shadow-md hover:bg-red-600"
-            >
-              {category.label} {/* Display the 'label' property */}
-            </Link>
+           <Link 
+           key={category.path} 
+           to={category.path.replace(":pageNo", "1")}
+           className="w-[180px] px-6 py-3 bg-red-500 text-white rounded-lg shadow-md hover:bg-red-600 text-center"
+         >
+           {category.label}
+         </Link>
+         
           ))}
 
         </div>
