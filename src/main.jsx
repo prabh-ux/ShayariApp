934cf34d52a19c6235864f108c32efce4312c8c3
prabh-ux <prabhkiratsinghbusiness@gmail.com>
2025-03-17 22:14:26 +0530

    added speed insights

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from "react-router-dom";
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
+
 import './index.css'
 import App from './App.jsx'
 
@@ -10,6 +12,7 @@ createRoot(document.getElementById('root')).render(
     <BrowserRouter>
       <App />
       <Analytics />
+      <SpeedInsights />
     </BrowserRouter>
   </StrictMode>
 )
