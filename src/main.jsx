d30092e13a85909d2143cb6676d23845b9107f07
prabh-ux <prabhkiratsinghbusiness@gmail.com>
2025-03-18 17:34:23 +0530

    fixed spped insight issue

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from "react-router-dom";
 import { Analytics } from "@vercel/analytics/react"
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/react"
+
 
 import './index.css'
 import App from './App.jsx'
